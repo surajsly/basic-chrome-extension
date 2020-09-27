1c1b62a420400187a0306f23ecf4ea57168be90a
Suraj Singh <50519406+surajsly@users.noreply.github.com>
2020-09-27 21:40:26 +0530

    show current time

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+/*
+data = document.getElementsByClassName("style-scope ytd-thumbnail-overlay-time-status-renderer");
+ */
+class App extends React.Component {
+  state = {
+    curTime: null,
+  };
+
+  componentDidMount() {
+    setInterval(() => {
+      this.setState({
+        curTime: new Date().toLocaleString(),
+      });
+    }, 1000);
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>{this.state.curTime}</p>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
