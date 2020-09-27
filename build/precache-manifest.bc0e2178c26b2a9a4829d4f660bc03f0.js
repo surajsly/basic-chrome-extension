0602c3897918b7d38d803767942b6280c649ae16
Suraj Singh <50519406+surajsly@users.noreply.github.com>
2020-09-27 23:59:40 +0530

    adding build

---
+++ build/precache-manifest.bc0e2178c26b2a9a4829d4f660bc03f0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a32699e49cee3b72fc21a26f2a333570",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a2bd0d3807d772e6eabe",
+    "url": "/static/css/main.c862fe32.chunk.css"
+  },
+  {
+    "revision": "40d4b4b1ddc6fbdfebab",
+    "url": "/static/js/2.2236cf2e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.2236cf2e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2bd0d3807d772e6eabe",
+    "url": "/static/js/main.9f14ad58.chunk.js"
+  },
+  {
+    "revision": "74e1238052e0c18b10a4",
+    "url": "/static/js/runtime-main.5bd30415.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
